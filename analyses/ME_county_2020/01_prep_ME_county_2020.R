626be2ddb3f2ea059b743ba4a68f0a886ca7a4c0
Christopher Kenny <ctk44@cornell.edu>
2025-10-27 15:30:45 -0400

    update maine

--- analyses/ME_county_2020/01_prep_ME_county_2020.R
+++ analyses/ME_county_2020/01_prep_ME_county_2020.R
@@ -29,7 +29,14 @@ if (!file_exists(here(state_path))) {
     rename_with(function(x) gsub('[0-9.]', '', x), starts_with('GEOID')) |>
     mutate(
       vap_oth = vap - vap_white - vap_black - vap_hisp,
-      vap_oth_b = vap_oth - vap_asian - vap_aian
+      vap_oth_b = vap_oth - vap_asian - vap_aian,
+      GEOID = str_sub(GEOID, 1, 11)
+    ) |>
+    group_by(GEOID) |>
+    summarize(
+      state = state[1],
+      county = county[1],
+      across(where(is.numeric), sum)
     )
 
   md <- read_lines(file = here(state_md_path))
